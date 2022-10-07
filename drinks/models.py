f624af3242a91c180a4d3c93d15215a3d92bb955
Emanuel Joseph <josephemanuel95@gmail.com>
2022-10-07 17:25:00 +0300

    initial commit

---
+++ drinks/models.py
@@ -0,0 +1,7 @@
+from statistics import mode
+from django.db import models
+
+class Drink(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    
